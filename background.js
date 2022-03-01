f399553e4686c81b6c0192e6f8022ff3f213d2bb
李云飞 <liyunfei02@daojia-inc.com>
2022-03-01 18:22:03 +0800

    取消弹窗插件

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('onInstalled');
+  // chrome.action.disable();
+})
---
+++ content.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  document.getElementById('loginModal').style.display = 'none';
+  document.querySelector('.modal-backdrop').style.display = 'none';
+}
